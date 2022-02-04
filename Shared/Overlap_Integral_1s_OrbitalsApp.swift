1f1d9bb8b3a1e6a146828ee2c2e40cde87dad022
Alaina Thompson <athompson9@hawk.iit.edu>
2022-02-04 10:19:11 -0600

    Initial Commit

---
+++ Shared/Overlap_Integral_1s_OrbitalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Overlap_Integral_1s_OrbitalsApp.swift
+//  Shared
+//
+//  Created by Alaina Thompson on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Overlap_Integral_1s_OrbitalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
